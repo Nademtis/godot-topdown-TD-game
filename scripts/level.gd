13af9e7c514c858d4d93d263ea8ea40054936db4
Nademtis <mathias.99.madsen@gmail.com>
2024-08-18 21:06:40 +0200

    wip on levelManager

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,6 +4,8 @@ extends Node
 @export var current_hp : int = 3
 @onready var ui = $UI
 
+func _ready():
+	levelManager.set_current_level(self.scene_file_path)
 
 func take_damage():
 	current_hp -= 1
--- scripts/levelManager.gd
+++ scripts/levelManager.gd
@@ -6,10 +6,14 @@ const LEVEL_EXTENSION = ".tscn"  # Scene file extension
 var current_scene = null
 
 func _ready():
-	print("Hello from LevelManager")
+	pass
 
 # Call this function when a level is complete
 func level_complete():
 	pass
 	# Access the current scene from the scene tree
 	# TODO swap levels
+
+func set_current_level(level):
+	current_scene = level #return res://scenes/levels/level1.tscn
+	#var current_index = leve
