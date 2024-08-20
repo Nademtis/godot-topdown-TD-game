f925450c45c9ba65d25a93378b560369547e821d
Nademtis <mathias.99.madsen@gmail.com>
2024-08-20 21:12:19 +0200

    log item slaps

--- scripts/item.gd
+++ scripts/item.gd
@@ -1,9 +1,20 @@
 class_name Item
 extends Node
 
+
+@onready var animation_player = $AnimationPlayer
+
 @export var item_name : String
 @export var weight : int 
+
+func _ready():
+	animation_player.play("spawned")
 
 func picked_up():
 	queue_free()
 
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == 'spawned':
+		animation_player.play("idle")
